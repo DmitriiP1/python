891454741f801fd09ab18eeba55592710043ba93
DmitriiP1 <36137274+DmitriiP1@users.noreply.github.com>
2018-03-22 22:26:45 +0300

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 from random import randint
 import argparse
-
+#Командный интерфейс
 parser = argparse.ArgumentParser()
 parser.add_argument('--length', type=int, help='Длина генерируемой последовательности.',required=True)
 parser.add_argument('--model', help='Путь к файлу, из которого загружается модель', required=True)
@@ -10,27 +10,30 @@ namespace = parser.parse_args()
 
 #Открываем результаты обработки текстов, заносим эти данные в словарь
 def readinput():
-    with open(namespace.model, 'r') as file:
-        dictionary_stat = {}
-        for line in file:
+    with open(namespace.model, 'r') as file:#дальше работаем с файлом путь к которому задана в model
+        dictionary_stat = {}#словарь со статистикой словосочетаний
+        for line in file:#обработка файла, в цикле будем доставать строчки из файла и доавлять их в словарь
             line = line.split()
-            line_str = str(str(line[0]) + ' ' + str(line[1]))
-            dictionary_stat.update({line_str : line[2]})
+            line_str = str(str(line[0]) + ' ' + str(line[1]))#строки в файле имеют вид: <слово1>' '<слово2> : статистика
+            dictionary_stat.update({line_str : line[2]})#статистика словосочетания - слово после второго пробела
         file.close()
         return dictionary_stat
 
-
+#Функция выдает слово, которое наиболее подходит для данного (через частоту встречающихся словосочетаний)
 def find_next_word(current_word, dictionary_stat):
-    candidats = []
-    for key in dictionary_stat:
-        temp = key.split()
-        if temp[0] == current_word:
+    candidats = []#кандидаты на искомое слово, список в виде словосочетаний(=ключи в словаре), затем по словосочетаний найдем статистику
+    for key in dictionary_stat: #перебираем ключи в словаре, ищем кандидатов и записываем статистику
+        temp = key.split()#временный список чтобы проверить первое слово в словосоечатнии из списка
+        if temp[0] == current_word:#проверка, если первое слово в словосочетанни = данному, то это словосочетание - кандидат
             candidats.append(key)
-    if len(candidats) == 0:
-        return 'WRONG'
+    if len(candidats) == 0:#если кандидаты не найдены, то текст завершается
+        return 'WRONG'#"Флаг, что алгоритм зашел в тупик
+    #Определение наиболее подходящего кандидата
+    #Сложим все значения в словаре, полученные по ключам(=найденные кандидаты)
+    #Рандом выдаст некоторое число, учитывая что в списке порядок элементов определен однозначно, можем найти натболее подходящего кандидадата
     sum = 0
     for word in candidats:
-        sum = sum + int(dictionary_stat[word])
+        sum = sum + int(dictionary_stat[word])#Сложим все значения в словаре, полученные по ключам(=найденные кандидаты)
     random_int = randint(0, sum)
     for candidat in candidats:
         temp = candidat.split()
@@ -38,37 +41,39 @@ def find_next_word(current_word, dictionary_stat):
             return temp[1]
         random_int = random_int - int(dictionary_stat[candidat])
 
-
+#Функция, генерирующая текст
+#На вход подается число слов в генерируемом тексте
+#Функция выдаст список слов в том порядке, в котором они должны стоять в тексте
 def generate(len_text):
-    dictionary_stat = readinput()
-    text = []
-    if namespace.seed == None:
-        start = randint(0, len(dictionary_stat))
+    dictionary_stat = readinput()#считываем статистику
+    text = []#Список для текста который выдаст функция
+    if namespace.seed == None:#если не задано первое слово, то выберем рандомное из списка
+        start = randint(0, len(dictionary_stat))#некоторое рандомное число
         pos = 0
-        word = ' '
-        for i in dictionary_stat:
+        word = ' '#первое слово
+        for i in dictionary_stat:#будем брать элементы из списка, пока не возьмем их столько, сколько выдал рандом
             if pos == start:
                 word = i.split()
                 word = word[0]
                 word = str(word)
                 break
             pos += 1
-    else:
+    else:#если первоначальное слово задано, то строим текст начиная с него
         word = namespace.seed
-    for i in range(len_text):
+    for i in range(len_text):#в цикле для i-го слова буем искать i + 1 при помощи полученой статистики
         text.append(word)
-        word = find_next_word(word, dictionary_stat)
-        if word == 'WRONG':
+        word = find_next_word(word, dictionary_stat)#определяем подходящее слово
+        if word == 'WRONG':#если подходящего слова нет, значит алгоритм защел в тупик
             return text
     return text
 
 
-text = []
-text = generate(namespace.length)
-if namespace.output != None:
-    file = open(namespace.output, 'w')
-    for i in text:
+text = []#генерируемый текст
+text = generate(namespace.length)#генерируем текст
+if namespace.output != None:#если указано в какой файл записать сгенерируемый текст, то запишем его туда
+    file = open(namespace.output, 'w')#указанный файл
+    for i in text:#запись в файл
         file.write(str(i) + ' ')
     file.close()
-else:
+else:#если не указано в какой файл записать сгенерируемый текст, то выведем его
     print(' '.join(text))
--- train.py
+++ train.py
@@ -1,36 +1,37 @@
 import re
 import os
 import argparse
-
+#Командный интерфейс
 parser = argparse.ArgumentParser()
 parser.add_argument('--input-dir', help='Путь к директории, в которой лежит коллекция документов', required=True, dest='dir')
 parser.add_argument('--model', help='Путь к файлу, в который сохраняется модель', required=True)
 parser.add_argument('--lc', help='Привести тексты к lowercase', action='store_const', const=True)
 namespace = parser.parse_args()
 
-files = os.listdir(namespace.dir)
-
+#Функция, обрабатывающая данный текст
+#На вход подается адрес файла с текстом
 def colibrate(fileread):
-    filewrite = open(namespace.model, 'w', encoding='utf-8')
-    fileread = open(fileread, 'r', encoding='utf-8')
-    dictionary_stat = {}
-    for line in fileread:
-        reg = re.compile('[^A-Za-zА-Яа-яё]')
-        tmp_line = reg.sub(' ', line)
-        if namespace.lc:
+    filewrite = open(namespace.model, 'w', encoding='utf-8')#Файл для записи статистики
+    fileread = open(fileread, 'r', encoding='utf-8')#Файл с текстом
+    dictionary_stat = {}#Словарь, хранящий статистику
+    for line in fileread:#Достаем часть текста
+        reg = re.compile('[^A-Za-zА-Яа-яё]')#Разрешенные символы
+        tmp_line = reg.sub(' ', line)#Удаляем лишние символы (оставляем нужные)
+        if namespace.lc:#Если в консоли попросили убрать заглавные буквы
             tmp_line = tmp_line.lower()
         tmp_line = tmp_line.split()
-        for i in range(len(tmp_line) - 1):
-            pair = tmp_line[i] + ' ' + tmp_line[i + 1]
-            dictionary_stat[pair] = dictionary_stat[pair] + 1 if pair in dictionary_stat else 1
-    for key in dictionary_stat:
+        for i in range(len(tmp_line) - 1):#Проходим по куску текста
+            pair = tmp_line[i] + ' ' + tmp_line[i + 1]#2 подряд идущие слова объединяем в словосочетание и записываем в словарь
+            dictionary_stat[pair] = dictionary_stat[pair] + 1 if pair in dictionary_stat else 1#Учитываем повторяющиеся словосочетания
+    for key in dictionary_stat:#Запись в файл полученной статистики
         filewrite.write(str(str(key) + ' ' + str(dictionary_stat[key]) +'\n'))
     fileread.close()
     filewrite.close()
 
-for i in files:
-    filename, file_extension = os.path.splitext(i)
-    if file_extension == '.txt':
-        temp = str(os.getcwd() + '/texts/' + str(i))
-        print(temp)
+files = os.listdir(namespace.dir)#список файлов в полученой директории
+for i in files:#рассматриваем каждый файл в директории
+    filename, file_extension = os.path.splitext(i)#разделяем имя файла на 2 части: заголовок и формат
+    if file_extension == '.txt':#если тхт, то обрабатываем его
+        temp = str(os.getcwd() + '/texts/' + str(i))#обрабатываемый файл
+        #print(temp)#выводим название обработываемого файла
         colibrate(temp)
