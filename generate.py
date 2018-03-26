c844d1841da90e49a049a236d922f5d5c0dbbcb9
DmitriiP1 <36137274+DmitriiP1@users.noreply.github.com>
2018-03-26 19:38:27 +0300

    Add files via upload
    Исправил ошибку "А B a B a" и прошел РЕР8

--- generate.py
+++ generate.py
@@ -2,80 +2,137 @@
 
 from random import randint
 import argparse
-#Командный интерфейс
+
+# Командный интерфейс
 parser = argparse.ArgumentParser()
-parser.add_argument('--length', type=int, help='Длина генерируемой последовательности.',required=True)
-parser.add_argument('--model', help='Путь к файлу, из которого загружается модель', required=True)
-parser.add_argument('--seed', help='Начальное слово')
-parser.add_argument('--output', help='Файл, в который будет записан результат')
+parser.add_argument('--length',
+                    type=int,
+                    help='Длина генерируемой последовательности.',
+                    required=True)
+
+parser.add_argument('--model',
+                    help='Путь к файлу, из которого загружается модель',
+                    required=True)
+
+parser.add_argument('--seed',
+                    help='Начальное слово')
+
+parser.add_argument('--output',
+                    help='Файл, в который будет записан результат')
+
 namespace = parser.parse_args()
 
-#Открываем результаты обработки текстов, заносим эти данные в словарь
+
+# Открываем результаты обработки текстов,
+# Заносим эти данные в словарь
 def readinput():
-    with open(namespace.model, 'r') as file:#дальше работаем с файлом путь к которому задана в model
-        dictionary_stat = {}#словарь со статистикой словосочетаний
-        for line in file:#обработка файла, в цикле будем доставать строчки из файла и доавлять их в словарь
+    # Дальше работаем с файлом путь к которому задана в model
+    with open(namespace.model, 'r') as file:
+        dictionary_stat = {}  # словарь со статистикой словосочетаний
+        # Обработка файла,
+        # В цикле будем доставать строчки из файла и доавлять их в словарь
+        for line in file:
             line = line.split()
-            line_str = str(str(line[0]) + ' ' + str(line[1]))#строки в файле имеют вид: <слово1>' '<слово2> : статистика
-            dictionary_stat.update({line_str : line[2]})#статистика словосочетания - слово после второго пробела
+
+            # Строки в файле имеют вид: <слово1>' '<слово2> : статистика
+            line_str = str(str(line[0]) + ' ' + str(line[1]))
+            # Статистика словосочетания - слово после второго пробела
+            dictionary_stat.update({line_str: line[2]})
+        # Закрываем файл model
         file.close()
+        # Словарь со статистикой
         return dictionary_stat
 
-#Функция выдает слово, которое наиболее подходит для данного (через частоту встречающихся словосочетаний)
+
+# Функция выдает слово, которое наиболее подходит для данного
+# (через частоту встречающихся словосочетаний)
 def find_next_word(current_word, dictionary_stat):
-    candidats = []#кандидаты на искомое слово, список в виде словосочетаний(=ключи в словаре), затем по словосочетаний найдем статистику
-    for key in dictionary_stat: #перебираем ключи в словаре, ищем кандидатов и записываем статистику
-        temp = key.split()#временный список чтобы проверить первое слово в словосоечатнии из списка
-        if temp[0] == current_word:#проверка, если первое слово в словосочетанни = данному, то это словосочетание - кандидат
+    # Кандидаты на искомое слово,
+    # Список в виде словосочетаний(=ключи в словаре),
+    candidats = []
+
+    # Перебираем ключи в словаре, ищем кандидатов и записываем статистику
+    for key in dictionary_stat:
+        # Проверка, если первое слово в словосочетанни = данному,
+        # то это словосочетание - кандидат
+        temp = key.split()
+        if temp[0] == current_word:
             candidats.append(key)
-    if len(candidats) == 0:#если кандидаты не найдены, то текст завершается
-        return 'WRONG'#"Флаг, что алгоритм зашел в тупик
-    #Определение наиболее подходящего кандидата
-    #Сложим все значения в словаре, полученные по ключам(=найденные кандидаты)
-    #Рандом выдаст некоторое число, учитывая что в списке порядок элементов определен однозначно, можем найти натболее подходящего кандидадата
+
+    # Если кандидаты не найдены, то текст завершается
+    if len(candidats) == 0:
+        # Флаг, что алгоритм зашел в тупик
+        return 'WRONG'
+
+    # Определение наиболее подходящего кандидата.
+    # Сложим все значения в словаре, полученные по ключам(=найденные кандидаты)
+    # Рандом выдаст некоторое число,
+    # учитывая что в списке порядок элементов определен однозначно,
+    # можем найти наиболее подходящего кандидадата
     sum = 0
     for word in candidats:
-        sum = sum + int(dictionary_stat[word])#Сложим все значения в словаре, полученные по ключам(=найденные кандидаты)
-    random_int = randint(0, sum)
-    for candidat in candidats:
-        temp = candidat.split()
-        if int(dictionary_stat[candidat]) >= random_int:
-            return temp[1]
-        random_int = random_int - int(dictionary_stat[candidat])
+        # Сложим все полученные по ключам значения в словаре
+        sum = sum + int(dictionary_stat[word])
 
-#Функция, генерирующая текст
-#На вход подается число слов в генерируемом тексте
-#Функция выдаст список слов в том порядке, в котором они должны стоять в тексте
+    random_int = randint(0, sum - 1)
+    for collocation in candidats:
+        temp = collocation.split()
+        if int(dictionary_stat[collocation]) >= random_int:
+            return temp[1]
+        random_int = random_int - int(dictionary_stat[collocation])
+
+
+# Функция, генерирующая текст
+# На вход подается число слов в генерируемом тексте
+# Выдает список слов в порядке, в котором они должны стоять в тексте
 def generate(len_text):
-    dictionary_stat = readinput()#считываем статистику
-    text = []#Список для текста который выдаст функция
-    if namespace.seed == None:#если не задано первое слово, то выберем рандомное из списка
-        start = randint(0, len(dictionary_stat))#некоторое рандомное число
-        pos = 0
-        word = ' '#первое слово
-        for i in dictionary_stat:#будем брать элементы из списка, пока не возьмем их столько, сколько выдал рандом
-            if pos == start:
+    # Считываем результаты тренировки
+    dictionary_stat = readinput()
+
+    # Список для текста, который выдаст функция
+    text = []
+
+    # Если не задано первое слово, то выберем рандомное из списка
+    if namespace.seed is None:
+        # Seed для слова
+        start = randint(0, len(dictionary_stat))
+        step = 0
+        # Первое слово
+        word = ' '
+        # Берем элементы из списка, столько, сколько выдал рандом
+        for i in dictionary_stat:
+            if step == start:
                 word = i.split()
                 word = word[0]
                 word = str(word)
                 break
-            pos += 1
-    else:#если первоначальное слово задано, то строим текст начиная с него
+            step += 1
+    # Если первоначальное слово задано, то строим текст начиная с него
+    else:
         word = namespace.seed
-    for i in range(len_text):#в цикле для i-го слова буем искать i + 1 при помощи полученой статистики
+
+    # В цикле для i-го слова буем искать i + 1 при помощи полученой статистики
+    for i in range(len_text):
         text.append(word)
-        word = find_next_word(word, dictionary_stat)#определяем подходящее слово
-        if word == 'WRONG':#если подходящего слова нет, значит алгоритм защел в тупик
+        # Определяем подходящее слово
+        word = find_next_word(word, dictionary_stat)
+        # Если подходящего слова нет, значит алгоритм зашел в тупик
+        if word == 'WRONG':
             return text
     return text
 
 
-text = []#генерируемый текст
-text = generate(namespace.length)#генерируем текст
-if namespace.output != None:#если указано в какой файл записать сгенерируемый текст, то запишем его туда
-    file = open(namespace.output, 'w')#указанный файл
-    for i in text:#запись в файл
+# Генерируем текст
+text = generate(namespace.length)
+# Если указано в какой файл записать сгенерируемый текст, то запишем его туда
+if namespace.output is not None:
+    # Указанный файл
+    file = open(namespace.output, 'w')
+    # Запись в файл
+
+    for i in text:
         file.write(str(i) + ' ')
     file.close()
-else:#если не указано в какой файл записать сгенерируемый текст, то выведем его
+# Если не указано в какой файл записать сгенерируемый текст, то выведем его
+else:
     print(' '.join(text))
--- train.py
+++ train.py
@@ -1,44 +1,87 @@
 # coding: utf-8
+
 import re
 import os
 import argparse
-#Командный интерфейс
+
+# Командный интерфейс
 parser = argparse.ArgumentParser()
-parser.add_argument('--input-dir', help='Путь к директории, в которой лежит коллекция документов', required=True, dest='dir')
-parser.add_argument('--model', help='Путь к файлу, в который сохраняется модель', required=True)
-parser.add_argument('--lc', help='Привести тексты к lowercase', action='store_const', const=True)
+parser.add_argument('--input-dir',
+                    help='Путь к директории с коллекцией документов',
+                    required=True,
+                    dest='dir')
+
+parser.add_argument('--model',
+                    help='Путь к файлу, в который сохраняется модель',
+                    required=True)
+
+parser.add_argument('--lc',
+                    help='Привести тексты к lowercase',
+                    action='store_const',
+                    const=True)
+
 namespace = parser.parse_args()
 
-#Функция, обрабатывающая данный текст
-#На вход подается адрес файла с текстом
+
+# Функция, обрабатывающая данный текст
+# На вход подается адрес файла с текстом
 def train(fileread):
-    filewrite = open(namespace.model, 'tw', encoding='utf-8')#Файл для записи статистики
-    fileread = open(fileread, 'r', encoding='utf-8')#Файл с текстом
-    dictionary_stat = {}#Словарь, хранящий статистику
-    for line in fileread:#Достаем часть текста
-        reg = re.compile('[^A-Za-zА-Яа-яё]')#Разрешенные символы
-        tmp_line = reg.sub(' ', line)#Удаляем лишние символы (оставляем нужные)
-        if namespace.lc:#Если в консоли попросили убрать заглавные буквы
+    # Файл для записи статистики
+    filewrite = open(namespace.model, 'tw', encoding='utf-8')
+    # Файл с текстом
+    fileread = open(fileread, 'r', encoding='utf-8')
+
+    # Словарь, хранящий статистику
+    dictionary_stat = {}
+    # Достаем часть текста
+    for line in fileread:
+        # Разрешенные символы
+        reg = re.compile('[^A-Za-zА-Яа-яё]')
+        # Удаляем лишние символы (оставляем нужные)
+        tmp_line = reg.sub(' ', line)
+
+        if namespace.lc:  # Если в консоли попросили убрать заглавные буквы
             tmp_line = tmp_line.lower()
+
         tmp_line = tmp_line.split()
-        for i in range(len(tmp_line) - 1):#Проходим по куску текста
-            pair = tmp_line[i] + ' ' + tmp_line[i + 1]#2 подряд идущие слова объединяем в словосочетание и записываем в словарь
-            dictionary_stat[pair] = dictionary_stat[pair] + 1 if pair in dictionary_stat else 1#Учитываем повторяющиеся словосочетания
-    for key in dictionary_stat:#Запись в файл полученной статистики
-        filewrite.write(str(str(key) + ' ' + str(dictionary_stat[key]) +'\n'))
+
+        # Проходим по куску текста
+        for i in range(len(tmp_line) - 1):
+            # 2 подряд идущие слова объединяем в словосочетание,
+            # записываем их в словарь
+            pair = tmp_line[i] + ' ' + tmp_line[i + 1]
+            # Учитываем повторяющиеся словосочетания
+            dictionary_stat[pair] = dictionary_stat[pair] + 1 \
+                if pair in dictionary_stat else 1
+
+    for key in dictionary_stat:  # Запись в файл полученной статистики
+        filewrite.write(str(str(key) + ' ' + str(dictionary_stat[key]) + '\n'))
+
+    # Закырваем файлы
     fileread.close()
     filewrite.close()
 
+
 def view_folder(path):
-    if os.path.isdir(path):#если файл - папка
+    # если файл - папка
+    if os.path.isdir(path):
+        # Путь к текущей директории
         link = os.path.normpath(path)
+        # Список фалов в текущей директории
         files = os.listdir(path)
-        for i in files:  # рассматриваем каждый файл в директории
-            view_folder(link + '/' + str(i))#открываем папку в текущей директории
 
-    filename, file_extension = os.path.splitext(path)#разделяем имя файла на 2 части: заголовок и формат
-    if file_extension == '.txt':#если тхт, то обрабатываем его
-        temp = str(os.path.normpath(path))#обрабатываемый файл
-        train(temp)
+        # Рассматриваем каждый файл в директории
+        for i in files:
+            # открываем папку в текущей директории
+            view_folder(link + '/' + str(i))
 
-view_folder(namespace.dir)#список файлов в полученой директории
+    # Разделяем имя файла на 2 части: заголовок и формат
+    filename, file_extension = os.path.splitext(path)
+    # Если .тхт, то обрабатываем его
+    if file_extension == '.txt':
+        # Обрабатываемый файл
+        file = str(os.path.normpath(path))
+        train(file)
+
+
+view_folder(namespace.dir)
