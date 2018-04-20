920ed1d637ef44a2c45ff97c361810cf45323ce2
DmitriiP1 <36137274+DmitriiP1@users.noreply.github.com>
2018-04-20 20:12:36 +0300

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,7 +1,13 @@
 # coding: utf-8
+# Генератор текстов
+# Павлов Дмитрий
+# МФТИ 2018
+# Ревью №1
+# Версия №0.1.0
 
 from random import randint
 import argparse
+import configparser
 
 # Командный интерфейс
 parser = argparse.ArgumentParser()
@@ -25,10 +31,11 @@ namespace = parser.parse_args()
 # Словарь со статистикой
 dictionary_stat = {}
 
-# Открываем результаты обработки текстов,
-# Заносим эти данные в словарь
-# Дальше работаем с файлом путь к которому задана в model
 with open(namespace.model, 'r') as file:
+    # Открываем результаты обработки текстов,
+    # Заносим эти данные в словарь
+    # Дальше работаем с файлом путь к которому задана в model
+
     # Обработка файла,
     # В цикле будем доставать строчки из файла и доавлять их в словарь
     for line in file:
@@ -42,9 +49,10 @@ with open(namespace.model, 'r') as file:
     file.close()
 
 
-# Функция выдает слово, которое наиболее подходит для данного
-# (через частоту встречающихся словосочетаний)
 def find_next_word(current_word, dictionary_stat):
+    # Функция выдает слово, которое наиболее подходит для данного
+    # (через частоту встречающихся словосочетаний)
+
     # Кандидаты на искомое слово,
     # Список в виде словосочетаний(=ключи в словаре),
     candidats = []
@@ -60,19 +68,19 @@ def find_next_word(current_word, dictionary_stat):
     # Если кандидаты не найдены, то текст завершается
     if len(candidats) == 0:
         # Флаг, что алгоритм зашел в тупик
-        return 'WRONG'
+        return None
 
     # Определение наиболее подходящего кандидата.
     # Сложим все значения в словаре, полученные по ключам(=найденные кандидаты)
     # Рандом выдаст некоторое число,
     # учитывая что в списке порядок элементов определен однозначно,
     # можем найти наиболее подходящего кандидадата
-    sum = 0
+    sum_values = 0
     for word in candidats:
         # Сложим все полученные по ключам значения в словаре
-        sum = sum + int(dictionary_stat[word])
+        sum_values = sum_values + int(dictionary_stat[word])
 
-    random_int = randint(0, sum - 1)
+    random_int = randint(0, sum_values - 1)
     for collocation in candidats:
         temp = collocation.split()
         if int(dictionary_stat[collocation]) >= random_int:
@@ -81,6 +89,8 @@ def find_next_word(current_word, dictionary_stat):
 
 
 def random_word():
+    # Выбирает рандомное слово
+
     # Seed для слова
     start = randint(0, len(dictionary_stat))
     step = 0
@@ -97,15 +107,16 @@ def random_word():
     return word
 
 
-# Функция, генерирующая текст
-# На вход подается число слов в генерируемом тексте
-# Выдает список слов в порядке, в котором они должны стоять в тексте
-def generate(len_text):
+def generate(len_text, seed):
+    # Функция, генерирующая текст
+    # На вход подается число слов в генерируемом тексте
+    # Выдает список слов в порядке, в котором они должны стоять в тексте
+
     # Список для текста, который выдаст функция
     text = []
     word = ''
     # Если не задано первое слово, то выберем рандомное из списка
-    if namespace.seed is None:
+    if seed is None:
         word = random_word()
     # Если первоначальное слово задано, то строим текст начиная с него
     else:
@@ -114,8 +125,8 @@ def generate(len_text):
         # Ищем есть ли seed среди ключей в словаре
         for key in dictionary_stat:
             temp = key.split()
-            if temp[0] == namespace.seed:
-                word = namespace.seed
+            if temp[0] == seed:
+                word = seed
                 # Нашли
                 flag = False
         if flag:
@@ -127,22 +138,25 @@ def generate(len_text):
         # Определяем подходящее слово
         word = find_next_word(word, dictionary_stat)
         # Если подходящего слова нет, значит алгоритм зашел в тупик
-        if word == 'WRONG' or word == '$':
+        if word is None or word == pair_for_last_word:
             return text
     return text
 
 
-# Генерируем текст
-text = generate(namespace.length)
-# Если указано в какой файл записать сгенерируемый текст, то запишем его туда
-if namespace.output is not None:
-    # Указанный файл
-    file = open(namespace.output, 'w')
-    # Запись в файл
-
-    for i in text:
-        file.write(str(i) + ' ')
-    file.close()
-# Если не указано в какой файл записать сгенерируемый текст, то выведем его
-else:
-    print(' '.join(text))
+if __name__ == '__main__':
+    # Чтение конфига
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+    pair_for_last_word = config['settings']['letter']
+    # Генерируем текст
+    text = generate(namespace.length, namespace.seed)
+    # Если указано в какой файл записать текст, то запишем его туда
+    if namespace.output is not None:
+        # Указанный файл
+        file = open(namespace.output, 'w')
+        # Запись в файл
+        file.write(' '.join(text))
+        file.close()
+    # Если не указано в какой файл записать текст, то выведем его
+    else:
+        print(' '.join(text))
