f1c299f355f40b2f53ff14b19d0e8dec6b2a77e1
DmitriiP1 <36137274+DmitriiP1@users.noreply.github.com>
2018-04-21 17:03:11 +0300

    Add files via upload

--- ForConfig.py
+++ ForConfig.py
@@ -1,6 +1,8 @@
 import configparser
 
 config = configparser.ConfigParser()
+# letter - символ, который будет показывать, что это слово было
+# последним в тексте и для него нельзя выбрать пару.
 config['settings'] = {'letter': '$'}
 with open('settings.ini', 'w') as configfile:
     config.write(configfile)
