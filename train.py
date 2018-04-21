6becd06e3ac962f3daae0eccd2c48a40ccf0459a
DmitriiP1 <36137274+DmitriiP1@users.noreply.github.com>
2018-04-21 17:02:41 +0300

    Add files via upload

--- train.py
+++ train.py
@@ -1,75 +1,103 @@
 # coding: utf-8
+# Создание модели по набору текстов
+# Павлов Дмитрий
+# МФТИ 2018
+# Ревью №1
+# Версия №0.1.0
 
 import re
 import os
 import argparse
+import configparser
+import json
 
 # Командный интерфейс
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Создание модели. Модель нужна '
+                                             'для генерации текстов '
+                                             'программой generate.py.')
 parser.add_argument('--input-dir',
-                    help='Путь к директории с коллекцией документов',
+                    help='Путь к директории с коллекцией .txt документов. '
+                         'По этим текстам программа обучится создавть свои '
+                         'тексты.',
                     required=True,
                     dest='dir')
 
 parser.add_argument('--model',
-                    help='Путь к файлу, в который сохраняется модель',
+                    help='Путь к файлу, в который сохраняется модель. '
+                         'Модель нужна для сохранения статистики, полученной '
+                         'после обучения.',
                     required=True)
 
 parser.add_argument('--lc',
-                    help='Привести тексты к lowercase',
+                    help='Привести тексты к нижнему регистру.',
                     action='store_const',
                     const=True)
 
+
 namespace = parser.parse_args()
 
-# Файл для записи статистики
-filewrite = open(namespace.model, 'tw', encoding='utf-8')
 
-# Словарь, хранящий статистику
-dictionary_stat = {}
+def add_pair(pair, dictionary_stat):
+    # Добавляет пару в модель, учитывая повторения
+
+    # pair - добавляемая пара слов
+
+    # dictionary_stat - словарь с моделью
+    # ------------------------------------------ #
+
+    if dictionary_stat.setdefault(pair) is None:
+        dictionary_stat[pair] = 1
+    else:
+        dictionary_stat[pair] += 1
 
 
-# Функция, обрабатывающая данный текст
-# На вход подается адрес файла с текстом
-def train(fileread):
+def train(file_for_train, lowercase, dictionary_stat):
+    # Функция, создающая модель на основе данного текст
+
+    # file_for_train - файл с текстом по которому будет обучаться программа
+    # lowercase - переменная, отвечающая за приведение слов, сохраняемых
+    # в модели, к нижнему регистру
+    # dictionary_stat - словарь с моделью
+    # ------------------------------------------------------------------- #
+
     # Файл с текстом
-    fileread = open(fileread, 'r', encoding='utf-8')
+    with open(file_for_train, 'r', encoding='utf-8') as file_for_train:
 
-    # Достаем часть текста
-    for line in fileread:
-        # Разрешенные символы
-        reg = re.compile('[^A-Za-zА-Яа-яё]')
-        # Удаляем лишние символы (оставляем нужные)
-        tmp_line = reg.sub(' ', line)
+        # Достаем часть текста
+        for line in file_for_train:
+            # Разрешенные символы
+            reg = re.compile('[^A-Za-zА-Яа-яё]')
+            # Удаляем лишние символы (оставляем нужные)
+            line = reg.sub(' ', line)
 
-        if namespace.lc:  # Если в консоли попросили убрать заглавные буквы
-            tmp_line = tmp_line.lower()
+            # Если в консоли попросили убрать заглавные буквы
+            if lowercase:
+                line = line.lower()
 
-        tmp_line = tmp_line.split()
+            line = line.split()
 
-        # Проходим по куску текста
-        for i in range(len(tmp_line) - 1):
-            # 2 подряд идущие слова объединяем в словосочетание,
-            # записываем их в словарь
-            pair = tmp_line[i] + ' ' + tmp_line[i + 1]
-            # Учитываем повторяющиеся словосочетания
-            dictionary_stat[pair] = dictionary_stat[pair] + 1 \
-                if pair in dictionary_stat else 1
-        pair = tmp_line[len(tmp_line) - 1] + ' ' + '$'
-        dictionary_stat[pair] = dictionary_stat[pair] + 1 \
-            if pair in dictionary_stat else 1
-
-    for key in dictionary_stat:  # Запись в файл полученной статистики
-        filewrite.write(str(str(key) + ' ' + str(dictionary_stat[key]) + '\n'))
-
-    # Закырваем файлы
-    fileread.close()
+            # Проходим по куску текста
+            for i in range(len(line) - 1):
+                # 2 подряд идущие слова объединяем в словосочетание,
+                # записываем их в словарь
+                pair = line[i] + ' ' + line[i + 1]
+                add_pair(pair, dictionary_stat)
+            if line:
+                pair = line[-1] + ' ' + pair_for_last_word
+                add_pair(pair, dictionary_stat)
 
 
-# Поиск .txt файлов в директории
-def find_txt_files(dir):
+def find_txt_files(directory, lowercase, dictionary_stat):
+    # Поиск .txt файлов в директории
+
+    # directory - директория с файлами для обучения
+    # lowercase - переменная, отвечающая за приведение слов, сохраняемых
+    # в модели, к нижнему регистру
+    # dictionary_stat - словарь с моделью
+    # ---------------------------------------------------------------- #
+
     # Поиск файлов в директории
-    for top, dirs, files in os.walk(dir):
+    for top, dirs, files in os.walk(directory):
         for i in files:
             # Путь к файлу
             path = str(os.path.join(top, i))
@@ -80,8 +108,27 @@ def find_txt_files(dir):
             if file_extension == '.txt':
                 # Обрабатываемый файл
                 file = str(os.path.normpath(path))
-                train(file)
+                train(file, lowercase, dictionary_stat)
 
 
-find_txt_files(namespace.dir)
-filewrite.close()
+if __name__ == '__main__':
+    # Словарь, хранящий статистику
+    dictionary_stat = {}
+
+    # Чтение конфига
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+    # letter - символ, который будет показывать, что это слово было
+    # последним в тексте и для него нельзя выбрать пару.
+    pair_for_last_word = config['settings']['letter']
+
+    # Очистим файл для запсии новой модели
+    open(namespace.model, 'w').close()
+
+    # Файл для записи модели
+    with open(namespace.model, 'a+', encoding='utf-8') as model:
+
+        find_txt_files(namespace.dir, namespace.lc, dictionary_stat)
+
+        # Запись в файл созданной модели
+        json.dump(dictionary_stat, model)
