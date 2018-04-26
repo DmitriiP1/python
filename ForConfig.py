7d504103a985f0ccdd521b9364daf3e52e9dd5b5
DmitriiP1 <36137274+DmitriiP1@users.noreply.github.com>
2018-04-26 11:39:43 +0300

    Delete ForConfig.py

--- ForConfig.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-# letter - символ, который будет показывать, что это слово было
-# последним в тексте и для него нельзя выбрать пару.
-config['settings'] = {'letter': '$'}
-with open('settings.ini', 'w') as configfile:
-    config.write(configfile)
